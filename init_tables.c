c1b1a1f453951e7c9f4d4a360a925c5686e775ba
sillvia95 <ungureanu.sylvia@gmail.com>
2015-12-30 17:30:15 +0200

    Inserted rows in Questions and Users tables

--- init_tables.c
+++ init_tables.c
@@ -56,6 +56,110 @@ int main(int argc, char* argv[]) {
         fprintf(stderr, "Table Users created succesfully\n");
     }
 
+    //Insert rows in Questions table
+    char *insert_questions = "INSERT INTO Questions (id, question, " \
+                             "right_answer, wrong_answer1, wrong_answer2, " \
+                             "wrong_answer3) " \
+                             "VALUES (1, 'Care este expresia generala pentru " \
+                             "toate sistemele de retea si de transfer fara " \
+                             "cablu?', 'a. Wireless', 'b. Infrarosu', 'c. Unde'," \
+                             "'d. Bluetooth');" \
+
+                             "INSERT INTO Questions (id, question, " \
+                             "right_answer, wrong_answer1, wrong_answer2, " \
+                             "wrong_answer3) " \
+                             "VALUES (2, 'Cum se numeste un mic fisier cu " \
+                             "informatii de tip text?', 'a. Cookie', " \
+                             "'b. Tools', 'c. History', 'd. Tab');" \
+
+                             "INSERT INTO Questions (id, question, " \
+                             "right_answer, wrong_answer1, wrong_answer2, " \
+                             "wrong_answer3) " \
+                             "VALUES (3, 'Cum se numeste o pagina de " \
+                             "internet?', 'a. Website', 'b. HTML', 'c. Web'," \
+                             "'d. Site');" \
+
+                             "INSERT INTO Questions (id, question, " \
+                             "right_answer, wrong_answer1, wrong_answer2, " \
+                             "wrong_answer3) " \
+                             "VALUES (4, 'Care este prescurtarea denumirii " \
+                             "vitezei de transfer pe biti pe secunda?', " \
+                             "'a. bps', 'b. tps', 'c. dps', 'd. mps');" \
+                             
+                             "INSERT INTO Questions (id, question, " \
+                             "right_answer, wrong_answer1, wrong_answer2, " \
+                             "wrong_answer3) " \
+                             "VALUES (5, 'Cum se numeste cel mai raspandit " \
+                             "limbaj de programare?', 'a. BASIC', 'b. C', " \
+                             "'c. C++', 'd. Pascal');" \
+
+                             "INSERT INTO Questions (id, question, " \
+                             "right_answer, wrong_answer1, wrong_answer2, " \
+                             "wrong_answer3) " \
+                             "VALUES (6, 'Cum se numeste o pagina de " \
+                             "internet?', 'a. Website', 'b. HTML', 'c. Web', " \
+                             "'d. Site');" \
+                             
+                             "INSERT INTO Questions (id, question, " \
+                             "right_answer, wrong_answer1, wrong_answer2, " \
+                             "wrong_answer3) " \
+                             "VALUES (7, 'Ce pasare poate imita vocile altor " \
+                             "pasari din padure?', 'a. Gaita', " \
+                             "'b. Papagalul', 'c. Vrabia', 'd. Mierla');" \
+
+                             "INSERT INTO Questions (id, question, " \
+                             "right_answer, wrong_answer1, wrong_answer2, " \
+                             "wrong_answer3) " \
+                             "VALUES (8, 'Cum se numeste pigmentul verde din " \
+                             "plante?', 'a. Clorofila', 'b. Petala', " \
+                             "'c. Frunza', 'd. Polen');" \
+
+                             "INSERT INTO Questions (id, question, " \
+                             "right_answer, wrong_answer1, wrong_answer2, " \
+                             "wrong_answer3) " \
+                             "VALUES (9, 'Ce copac este considerat sacru in " \
+                             "China?', 'a. Gingko', 'b. Bambusul', " \
+                             "'c. Pinul', 'd. Gorunul');" \
+
+                             "INSERT INTO Questions (id, question, " \
+                             "right_answer, wrong_answer1, wrong_answer2, " \
+                             "wrong_answer3) " \
+                             "VALUES (10, 'Unde se afla Waterloo?', " \
+                             "'a. Belgia', 'b. Franta', 'c. Anglia', " \
+                             "'d.Austria');";                         
+    
+    // Execute third statement
+    if (sqlite3_exec(db, insert_questions, callback, 0, &zErrMsg) != SQLITE_OK) {
+        fprintf(stderr, "Could not insert rows in Questions table.\nSQL error: %s\n",
+                         zErrMsg);
+        sqlite3_free(zErrMsg);
+        exit(0);
+    } else {
+        fprintf(stderr, "Rows inserted succesfully\n");
+    }
+    //Insert rows in Users table
+    char* insert_users = "INSERT INTO Users (user, password, " \
+                         "nr_of_games_played) " \
+                         "VALUES (silvia95, anamaria96, 2);" \
+
+                         "INSERT INTO Users (user, password, " \
+                         "nr_of_games_played) " \
+                         "VALUES (gadyPhol, apollo18, 1);" \
+
+                         "INSERT INTO Users (user, password, " \
+                         "nr_of_games_played) " \
+                         "VALUES (silvia, 123456, 2);" ;
+
+    // Execute last statement
+    if (sqlite3_exec(db, insert_users, callback, 0, &zErrMsg) != SQLITE_OK) {
+        fprintf(stderr, "Couldn't insert rows in Users table.\nSQL error: %s\n",
+                zErrMsg);
+        sqlite3_free(zErrMsg);
+        exit(0);
+    } else {
+        fprintf(stderr, "Rows inserted succesfully\n");
+    }
+                         
     sqlite3_close(db);
     return 0;
 }
